1952ad22f35fed1402ca177a6d66c17cea12a540
Rogério Gouvêa <rogeriog.em@gmail.com>
2025-07-01 13:28:55 +0200

    Delete build directory

--- build/lib/mattervial/featurizers/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-from .structure import *
-from .composition import *
-from .sisso_featurization import *
-
-__all__ = structure.__all__
-__all__ += composition.__all__
-__all__ += sisso_featurization.__all__
